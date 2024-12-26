8bb8ed5548fcf09652b07dece300427095e589f6
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2024-12-26 20:16:28 +1000

    Added ollama server and LLM model

--- Aspire.Llm.AppHost/Program.cs
+++ Aspire.Llm.AppHost/Program.cs
@@ -1,3 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Add Ollama server
+var ollama =
+    builder.AddOllama("ollama")
+        .WithDataVolume()
+        .WithOpenWebUI();
+
+// Add chat model
+var chat = ollama.AddModel("chat", "llama3.2");
+
+builder.AddProject<Projects.Aspire_Llm_Console>("console")
+    .WithReference(chat)
+    .WaitFor(chat);
+
 builder.Build().Run();
---
+++ readme.md
@@ -0,0 +1,5 @@
+# .NET Aspire with LLM's
+
+## Resources
+
+- https://devblogs.microsoft.com/dotnet/local-ai-models-with-dotnet-aspire/
