014910d6687b7206cd36a9c7bec2968afb54b165
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2024-12-27 10:47:50 +1000

    Got got Ollama working with phi3.5 and llama3.2.

--- Aspire.Llm.AppHost/Program.cs
+++ Aspire.Llm.AppHost/Program.cs
@@ -2,15 +2,33 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 // Add Ollama server
 var ollama =
-    builder.AddOllama("ollama")
-        .WithDataVolume()
-        .WithOpenWebUI();
+        builder
+            .AddOllama("ollama", 2200) // 2200
+            // .WithGPUSupport()
+    // .WithContainerRuntimeArgs("--gpus=all");
+            .WithDataVolume()
+            .WithOpenWebUI()
+    .AddModel("chat", AiModels.Llama)
+    ;
+
+// ollama
+    // .AddModel("chat", "phi3.5-mini-4k-instruct")
+    // .port(5000);
+    ;
 
 // Add chat model
-var chat = ollama.AddModel("chat", "llama3.2");
+// var chat = ollama.AddModel("chat", "llama3.2");
 
-builder.AddProject<Projects.Aspire_Llm_Console>("console")
-    .WithReference(chat)
-    .WaitFor(chat);
+// builder.AddProject<Projects.Aspire_Llm_Console>("console")
+//     .WithReference(chat)
+//     .WaitFor(chat);
 
 builder.Build().Run();
+
+public static class AiModels
+{
+    public const string Llama = "llama3.2";
+    public const string AllMiniLm = "all-minilm";
+    public const string Phi3Mini = "phi3.5";
+    public const string Gpt40Mini = "gpt-4o-mini";
+}
--- Aspire.Llm.Console/Program.cs
+++ Aspire.Llm.Console/Program.cs
@@ -1,16 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-using Aspire.Llm.Console.Commands;
-using Cocona;
+var builder = Host.CreateApplicationBuilder();
 
-var builder = CoconaApp.CreateBuilder();
+// Add in if embedding generator is needed
+// builder.AddOllamaSharpEmbeddingGenerator("chat");
+
+builder.AddKeyedOllamaSharpChatClient("chat", s =>
+{
+    s.Endpoint = new Uri("http://localhost:2200");
+    s.SelectedModel = "phi3.5";
+});
+
+builder.Services.AddChatClient(sp => sp.GetRequiredKeyedService<IChatClient>("chat"))
+    .UseFunctionInvocation()
+    .UseOpenTelemetry(configure: t => t.EnableSensitiveData = true)
+    .UseLogging();
+
+    // Use the OllamaSharp client
+    // .Use());
+
+
+// builder.Services.AddChatClient( )
+
+
 var app = builder.Build();
+
+var chatClient = app.Services.GetRequiredKeyedService<IChatClient>("chat");
+
+// Add messages for memory
+var messages = new List<ChatMessage>();
+
+string input;
+do
+{
+    Console.Write("Query: ");
+    input = Console.ReadLine();
+    if (!string.IsNullOrEmpty(input))
+    {
+        messages.Add(new ChatMessage { Text = input });
+        var response = await chatClient.CompleteAsync(messages);
+        messages.AddRange(response.Choices);
+        Console.WriteLine("Result: " + response.Message);
+    }
+} while (!string.IsNullOrEmpty(input));
+
+Console.WriteLine("Exiting...");
+
+// app.Run();
 
 // app.AddCommand((string name, int age) =>
 // {
 //     Console.WriteLine("Hello, World, name: {0}, age: {1}", name, age);
 // });
 
-app.AddCommands<MyCommand>();
-
-app.Run();
+// app.AddCommands<MyCommand>();
